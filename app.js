1210b1767ccf6b37986fb76c50599818f55cefcf
Derek Royce Luy Burias <derek.burias@gmail.com>
2024-03-14 07:12:47 +0800

    misc progress (cramming master)
    removed _old files from repo
    login register progress, starting to implement store pages

--- app.js
+++ app.js
@@ -25,21 +25,69 @@ function errorFn(err) {
 }
 
 // TODO: CREATE SCHEMA
+const loginSchema = new mongoose.Schema({
+    user: { type: String },
+    pass: { type: String }
+}, { versionKey: false });
+
+const loginModel = mongoose.model('login', loginSchema)
+
 // SAMPLE POST SCHEMA
 const postSchema = new mongoose.Schema({
     post_title: { type: String },
-    post_image: { type: String }
-}, { versionKey: false});
+    post_user: { type: String },
+    post_store: { type: String },
+    post_content: { type: String }
+}, { versionKey: false });
 
 const postModel = mongoose.model('post', postSchema); // NOTE: becomes "posts"
 
 // TODO
 server.get('/', function(req, resp) {
-    resp.render('main', {
-        layout: 'index',
-        title: 're*curate'
+    resp.render('registration', {
+        layout: 'form',
+        title: 'register to re*curate'
     });
 });
+
+server.post('/create-user', function(req, resp) {
+    const loginInstance = loginModel({
+        user: req.body.user,
+        pass: req.body.pass
+    });
+
+    loginInstance.save().then(function(login) {
+        console.log('User created');
+
+        resp.render('login', {
+            layout: 'form',
+            title: 're*curate login'
+        });
+    }).catch(errorFn);
+});
+
+server.post('/read-user', function(req, resp) {
+    const searchQuery = { user: req.body.user, pass: req.body.pass };
+
+    loginModel.findOne(searchQueary).then(function(login){
+        console.log('Finding user');
+
+        if (login != undefined && login._id != null) {
+            resp.render('index', {
+                layout: 'main',
+                title: 're*curate'
+            });
+        } else {
+            alert('Failed to log in. Please check username and password.');
+        }
+    }).catch(errorFn);
+})
+
+// TODO: create storepages
+server.get('/store/:storename', function(req, resp) {
+    const storename = req.params.storename;
+    resp.send()
+})
 
 /* SAMPLE (take note of post_data and friend_data):
   postModel.find(searchQuery).lean().then(function(post_data){
@@ -53,6 +101,16 @@ server.get('/', function(req, resp) {
     }).catch(errorFn);
   }).catch(errorFn);
 */
+
+function finalClose() {
+    console.log('Close connection at the end!');
+    mongoose.connection.close();
+    process.exit();
+}
+
+process.on('SIGTERM', finalClose);
+process.on('SIGINT', finalClose);
+process.on('SIGQUIT', finalClose);
 
 const port = process.env.PORT | 3000;
 server.listen(port, function() {
